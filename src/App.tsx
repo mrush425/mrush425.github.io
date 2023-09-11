c02fbb3e641e256b89b318f0eaca7ce30206a1db
The Walrus <mrush425@gmail.com>
2023-09-10 20:53:25 -0500

    landing page for future website

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 const user = {
   name: 'Future Home of the League of the Trolls Website',
   imageUrl: 'https://images.unsplash.com/photo-1627477150479-b7f109c3aaa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80',
-  imageSize: 90,
+  imageSize: 200,
 };
 
 function App() {
