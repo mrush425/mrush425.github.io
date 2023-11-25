c03442925d00bb5fe2188f9cdeb19ae1c368fbd5
The Walrus <mrush425@gmail.com>
2023-11-24 22:12:01 -0600

    Fixed styling and added colors to schedule comparison table, haven't tested going to other seasons

--- src/Pages/Year Pages/ScheduleComparison.tsx
+++ src/Pages/Year Pages/ScheduleComparison.tsx
@@ -14,29 +14,28 @@ interface ScheduleComparisonProps {
 
 
 const ScheduleComparison: React.FC<ScheduleComparisonProps> = ({ data }) => {
-  const [matchupInfo, setMatchupInfo] = useState<MatchupInfo[]>([]);
+  let [matchupInfo, setMatchupInfo] = useState<MatchupInfo[]>([]);
   const [dataFetched, setDataFetched] = useState(false);
-  
-  
-    useEffect(() => {
-      // Fetch league data when the component mounts
-      const fetchMatchupData = async () => {
-        try {
-          const info = await getMatchupData(data);
-          setMatchupInfo(info);
-          data.matchupInfo=info;
-          setDataFetched(true);
-        } catch (error) {
-          console.error('Error fetching league data:', error);
-        }
-      };
-      if(data.matchupInfo===undefined){
-      fetchMatchupData();
-      }
-      else{
+
+  useEffect(() => {
+    const fetchMatchupData = async () => {
+      try {
+        const info = await getMatchupData(data);
+        setMatchupInfo(info);
+        data.matchupInfo = info;
         setDataFetched(true);
+      } catch (error) {
+        console.error('Error fetching league data:', error);
+        setDataFetched(true); // Set dataFetched to true even in case of an error to avoid infinite loading
       }
-    }, []); // Run this effect only once on mount
+    };
+
+    if (data.matchupInfo === undefined) {
+      fetchMatchupData();
+    } else {
+      setDataFetched(true);
+    }
+  }, [data, matchupInfo]);
 
   if (!dataFetched) {
     // Render a loading indicator or placeholder while data is being fetched
@@ -47,6 +46,10 @@ const ScheduleComparison: React.FC<ScheduleComparisonProps> = ({ data }) => {
     let wins: number = 0;
     let losses: number = 0;
     let ties: number = 0;
+
+    if (!data.matchupInfo) {
+      return [0, 0, 0]; // or handle it differently based on your use case
+    }
 
     if(team.user_id===schedule.user_id){
       const roster = findRosterByUserId(team.user_id,data.rosters);
@@ -61,15 +64,17 @@ const ScheduleComparison: React.FC<ScheduleComparisonProps> = ({ data }) => {
       // Find the matchupInfo for the current team and schedule
       let teamRosterId = findRosterByUserId(team.user_id, data.rosters)?.roster_id;
       let scheduleRosterId = findRosterByUserId(schedule.user_id, data.rosters)?.roster_id;
-    
-      const relevantMatchups = matchupInfo.filter(
+      
+      const relevantMatchups = data.matchupInfo.filter(
         (matchup) =>
           matchup.week !== data.nflSeasonInfo.week && // Exclude matchups for the current week
           matchup.matchups.some((m) => m.roster_id === teamRosterId) &&
           matchup.matchups.some((m) => m.roster_id === scheduleRosterId)
       );
+      
     
       relevantMatchups.forEach((matchup) => {
+        
         const teamMatchup = matchup.matchups.find((m) => m.roster_id === teamRosterId);
         const scheduleMatchup = matchup.matchups.find((m) => m.roster_id === scheduleRosterId);
         const oppMatchup = matchup.matchups.find((m) => m.matchup_id === scheduleMatchup?.matchup_id && m.roster_id!== scheduleMatchup?.roster_id); 
@@ -107,36 +112,64 @@ const ScheduleComparison: React.FC<ScheduleComparisonProps> = ({ data }) => {
       recordString = wins + "-" + losses + "-" + ties;
     }
 
-    if(schedule.metadata.team_name==="TyReeking Havoc"){
-      console.log("Team Name: " + team.metadata.team_name + " Schedule: " + schedule.metadata.team_name + " " + recordString);
-    }
-
     return recordString;
   }
 
+  const findExtremeRecords = (records: string[]): number[] => {
+    const parsedRecords = records.map((record) => record.split('-').map(Number));
+
+    const maxRecord = Math.max(...parsedRecords.map(([wins]) => wins));
+    const minRecord = Math.min(...parsedRecords.map(([wins]) => wins));
+
+    return [maxRecord, minRecord];
+  };
+
+  const getCellStyle = (record: string, extremeRecords: number[]): React.CSSProperties => {
+    const parsedRecord = record.split('-').map(Number);
+    const [wins] = parsedRecord;
+
+    if (wins === extremeRecords[0]) {
+      // Best record, apply softer green background
+      return { backgroundColor: '#9ae59a'};
+    } else if (wins === extremeRecords[1]) {
+      // Worst record, apply softer red background
+      return { backgroundColor: '#ff9999'};
+    }
+
+    // Default styling
+    return {};
+  };
+
   return (
-    <div>
+    <div className="schedule-comparison-container">
       <YearNavBar data={data} />
-      <table>
+      <table className="schedule-table schedule-comparison-table">
         <thead>
+          <tr>
             <th></th>
             <th><b>Vs Schedule</b></th>
+          </tr>
         </thead>
         <tbody>
           <tr>
             <td><b>Team Name</b></td>
             {data.users.map((user, index) => (
-              <td key={index+1}>{user.metadata.team_name}</td>
+              <td key={index + 1}>{user.metadata.team_name}</td>
             ))}
           </tr>
           {data.users.map((user, rowIndex) => (
             <tr key={rowIndex}>
               <td>{user.metadata.team_name}</td>
               {data.users.map((_, colIndex) => (
-                <td key={colIndex}>
+                <td key={colIndex} style={getCellStyle(
+                  displayRecord(data.users[rowIndex], data.users[colIndex]),
+                  findExtremeRecords(data.users.map((_, innerColIndex) =>
+                    displayRecord(data.users[rowIndex], data.users[innerColIndex])
+                  ))
+                )}>
                   {displayRecord(
-                    data.users[rowIndex],  // Current team (row)
-                    data.users[colIndex]   // Schedule for the current column
+                    data.users[rowIndex], // Current team (row)
+                    data.users[colIndex] // Schedule for the current column
                   )}
                 </td>
               ))}
