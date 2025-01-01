79144c4d026c29c206ab4e886dba674b32afe163
The Walrus <mrush425@gmail.com>
2025-01-01 17:09:26 -0600

    Created hall of fame home page with winners/losers

--- src/Pages/HallOfFamePages/HallOfFameHome.tsx
+++ src/Pages/HallOfFamePages/HallOfFameHome.tsx
@@ -1,18 +1,76 @@
 import React from 'react';
-import '../../Stylesheets/Hall of Fame Stylesheets/HallOfFameHome.css'
+import '../../Stylesheets/Hall of Fame Stylesheets/HallOfFameHome.css';
 import LeagueData from '../../Interfaces/LeagueData';
 import HallOfFameNavBar from '../../Navigation/HallOfFameNavBar';
 import HallOfFameProps from './HallOfFameProps';
 
-
 const HallOfFameHome: React.FC<HallOfFameProps> = ({ data }) => {
+  const safeImage = (path: string) => {
+    try {
+      return require(`${path}`);
+    } catch {
+      return null;
+    }
+  };
+
   return (
     <div>
-      <HallOfFameNavBar data={data} /> 
+      <HallOfFameNavBar data={data} />
       <h2>Welcome to the Hall of Fame</h2>
-      <div>Put league winners and losers pictures here</div>
+      <table className="hall-of-fame-table">
+        <thead>
+          <tr>
+            <th className="header-column"></th>
+            {data.map((league) => (
+              <th key={league.season} className="header-column">
+                {league.season}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="year-column">Champion</td>
+            {data.map((league) => {
+              const championImage = safeImage(`./Champions/${league.season}.jpg`);
+              return (
+                <td key={league.season}>
+                  {championImage ? (
+                    <img
+                      src={championImage}
+                      alt={`${league.season} Champion`}
+                      className="champion-image"
+                    />
+                  ) : (
+                    <span>No Image</span>
+                  )}
+                </td>
+              );
+            })}
+          </tr>
+          <tr>
+            <td className="year-column">Butler</td>
+            {data.map((league) => {
+              const butlerImage = safeImage(`./Butlers/${league.season}.jpg`);
+              return (
+                <td key={league.season}>
+                  {butlerImage ? (
+                    <img
+                      src={butlerImage}
+                      alt={`${league.season} Butler`}
+                      className="butler-image"
+                    />
+                  ) : (
+                    <span>No Image</span>
+                  )}
+                </td>
+              );
+            })}
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default HallOfFameHome;
