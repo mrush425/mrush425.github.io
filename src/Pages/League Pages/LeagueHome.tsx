35a9633a2dcce990cfa605216802e201f7a37b5b
The Walrus <mrush425@gmail.com>
2025-01-04 22:51:42 -0600

    Added bowl winners to League Stats home page

--- src/Pages/League Pages/LeagueHome.tsx
+++ src/Pages/League Pages/LeagueHome.tsx
@@ -2,13 +2,47 @@ import React from 'react';
 import '../../Stylesheets/League Stylesheets/LeagueHome.css';
 import LeagueProps from './LeagueProps';
 import LeagueNavBar from '../../Navigation/LeagueNavBar';
+import { getBowlWinner } from '../../Helper Files/HelperMethods';
 
 const LeagueHome: React.FC<LeagueProps> = ({ data }) => {
+  const bowlNames = [
+    "Troll Bowl",
+    "Bengal Bowl",
+    "Koozie Bowl",
+    "Toilet Bowl",
+    "Diarrhea Bowl",
+    "Butler Bowl",
+  ];
 
   return (
     <div>
       <LeagueNavBar data={data} />
       <h2>Overall League Stats (Under Construction)</h2>
+      <table className="league-stats-table">
+        <thead>
+          <tr>
+            <th>Season</th>
+            {bowlNames.map((bowl) => (
+              <th key={bowl}>{bowl}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((league, index) => (
+            <tr key={index}>
+              <td>{league.season}</td>
+              {bowlNames.map((bowl) => {
+                const [winner] = getBowlWinner(bowl, league);
+                return (
+                  <td key={bowl}>
+                    {winner ? winner.metadata.team_name : "N/A"}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
