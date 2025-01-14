3983f9b66fd47c851d168020c97a25450f78bd2d
The Walrus <mrush425@gmail.com>
2025-01-13 19:18:03 -0600

    Fix bug with columns due to using bootstrap

--- src/Components/MatchupDisplay.tsx
+++ src/Components/MatchupDisplay.tsx
@@ -25,19 +25,13 @@ const MatchupDisplay: React.FC<MatchupDisplayProps> = ({
 }) => {
   const isDefaultState = !user1 || !user2 || !data;
 
-  /**
-   * Calculates the user's score for the specified week.
-   */
   const getUserScore = (user: SleeperUser | null, week: number): number => {
-    if (!user || !data) return 0; // Default score if data or user is missing
+    if (!user || !data) return 0;
     return getScoreForWeek(user, week, data);
   };
 
   if (isDefaultState) return <div>Please Select a Matchup</div>;
 
-  /**
-   * Renders the player's name or fallback if unavailable.
-   */
   const renderStarter = (playerId: string | undefined): string => {
     if (!playerId) return '';
     if (playerId in playerData) {
@@ -47,17 +41,12 @@ const MatchupDisplay: React.FC<MatchupDisplayProps> = ({
     return playerId;
   };
 
-  /**
-   * Renders the player's image or fallback if unavailable.
-   */
   const renderStarterImage = (starter: string | undefined): JSX.Element | null => {
     if (!starter) return null;
-
     const isPlayer = !isNaN(Number(starter));
     const imageUrl = isPlayer
       ? `https://sleepercdn.com/content/nfl/players/${starter}.jpg`
       : `https://sleepercdn.com/images/team_logos/nfl/${starter.toLowerCase()}.png`;
-
     const fallbackImageUrl = isPlayer
       ? 'https://sleepercdn.com/images/fallback_player_image.png'
       : 'https://sleepercdn.com/images/fallback_team_logo.png';
@@ -72,9 +61,6 @@ const MatchupDisplay: React.FC<MatchupDisplayProps> = ({
     );
   };
 
-  /**
-   * Renders team information including avatar and score.
-   */
   const renderTeamInfo = (
     user: SleeperUser | null,
     defaultTeamName: string,
@@ -86,7 +72,7 @@ const MatchupDisplay: React.FC<MatchupDisplayProps> = ({
       ? `https://sleepercdn.com/avatars/${user.avatar}`
       : user.metadata.avatar
       ? user.metadata.avatar
-      : require('./Images/Default Team.png'); // Fallback to local image if no avatar
+      : require('./Images/Default Team.png');
 
     const score = secondWeek
       ? getUserScore(user, week) + getUserScore(user, secondWeek)
@@ -94,26 +80,23 @@ const MatchupDisplay: React.FC<MatchupDisplayProps> = ({
 
     return isLeftSide ? (
       <>
-        <th className="col-10">
+        <th className="matchup-col-10">
           <img src={avatarUrl} alt={`${user.metadata.team_name || defaultTeamName} avatar`} className="team-avatar" />
         </th>
-        <th className="col-28">{user.metadata.team_name || defaultTeamName}</th>
-        <th className="col-10">{score.toFixed(2)}</th>
+        <th className="matchup-col-28">{user.metadata.team_name || defaultTeamName}</th>
+        <th className="matchup-col-10">{score.toFixed(2)}</th>
       </>
     ) : (
       <>
-        <th className="col-10">{score.toFixed(2)}</th>
-        <th className="col-28">{user.metadata.team_name || defaultTeamName}</th>
-        <th className="col-10">
+        <th className="matchup-col-10">{score.toFixed(2)}</th>
+        <th className="matchup-col-28">{user.metadata.team_name || defaultTeamName}</th>
+        <th className="matchup-col-10">
           <img src={avatarUrl} alt={`${user.metadata.team_name || defaultTeamName} avatar`} className="team-avatar" />
         </th>
       </>
     );
   };
 
-  /**
-   * Renders individual rows for the matchup.
-   */
   const renderMatchupRows = (
     matchup1: Matchup | undefined,
     matchup2: Matchup | undefined,
@@ -124,32 +107,28 @@ const MatchupDisplay: React.FC<MatchupDisplayProps> = ({
         <td colSpan={7}></td>
       </tr>,
       <tr key={`row-${i}`} className="data-row">
-        <td className="col-10">{renderStarterImage(matchup1?.starters[i])}</td>
-        <td className="col-28">{renderStarter(matchup1?.starters[i])}</td>
-        <td className="col-10">{matchup1?.starters_points[i]?.toFixed(2) || ''}</td>
-        <td className="col-4">{data?.roster_positions[i] || ''}</td>
-        <td className="col-10">{matchup2?.starters_points[i]?.toFixed(2) || ''}</td>
-        <td className="col-28">{renderStarter(matchup2?.starters[i])}</td>
-        <td className="col-10">{renderStarterImage(matchup2?.starters[i])}</td>
+        <td className="matchup-col-10">{renderStarterImage(matchup1?.starters[i])}</td>
+        <td className="matchup-col-28">{renderStarter(matchup1?.starters[i])}</td>
+        <td className="matchup-col-10">{matchup1?.starters_points[i]?.toFixed(2) || ''}</td>
+        <td className="matchup-col-4">{data?.roster_positions[i] || ''}</td>
+        <td className="matchup-col-10">{matchup2?.starters_points[i]?.toFixed(2) || ''}</td>
+        <td className="matchup-col-28">{renderStarter(matchup2?.starters[i])}</td>
+        <td className="matchup-col-10">{renderStarterImage(matchup2?.starters[i])}</td>
       </tr>,
     ]);
 
-  /**
-   * Renders the total points row.
-   */
   const renderTotalPointsRow = (matchup1: Matchup | undefined, matchup2: Matchup | undefined): JSX.Element => (
     <tr className="total-points-row">
-      <td className="col-10"></td>
-      <td className="col-28"></td>
-      <td className="col-10 total-points-col">{matchup1?.points?.toFixed(2) || ''}</td>
-      <td className="col-4"></td>
-      <td className="col-10 total-points-col">{matchup2?.points?.toFixed(2) || ''}</td>
-      <td className="col-28"></td>
-      <td className="col-10"></td>
+      <td className="matchup-col-10"></td>
+      <td className="matchup-col-28"></td>
+      <td className="matchup-col-10 total-points-col">{matchup1?.points?.toFixed(2) || ''}</td>
+      <td className="matchup-col-4"></td>
+      <td className="matchup-col-10 total-points-col">{matchup2?.points?.toFixed(2) || ''}</td>
+      <td className="matchup-col-28"></td>
+      <td className="matchup-col-10"></td>
     </tr>
   );
 
-  // Data preparation
   const user1matchup1 = getMatchupForWeek(user1, week, data);
   const user2matchup1 = getMatchupForWeek(user2, week, data);
   const user1matchup2 = secondWeek ? getMatchupForWeek(user1, secondWeek, data) : undefined;
@@ -163,7 +142,7 @@ const MatchupDisplay: React.FC<MatchupDisplayProps> = ({
         <thead className="matchup-header">
           <tr className="header-row">
             {renderTeamInfo(user1, 'Team 1', true)}
-            <th className="col-4" colSpan={1}>
+            <th className="matchup-col-4" colSpan={1}>
               VS
             </th>
             {renderTeamInfo(user2, 'Team 2', false)}
@@ -174,22 +153,17 @@ const MatchupDisplay: React.FC<MatchupDisplayProps> = ({
         </thead>
         <tbody>
           {renderMatchupRows(user1matchup1, user2matchup1, maxStartersCount)}
-
           {secondWeek && renderTotalPointsRow(user1matchup1, user2matchup1)}
-
           {secondWeek && (
             <>
-            <tr className="empty-row">
+              <tr className="empty-row">
                 <td colSpan={7}></td>
-            </tr>
-            <tr className="empty-row">
-                <td colSpan={7}></td>
-            </tr>
-            <tr className="second-week-header">
+              </tr>
+              <tr className="second-week-header">
                 <td colSpan={7}>Second Week Matchup</td>
-            </tr>
-                {renderMatchupRows(user1matchup2, user2matchup2, maxStartersCount)}
-                {renderTotalPointsRow(user1matchup2, user2matchup2)}
+              </tr>
+              {renderMatchupRows(user1matchup2, user2matchup2, maxStartersCount)}
+              {renderTotalPointsRow(user1matchup2, user2matchup2)}
             </>
           )}
         </tbody>
